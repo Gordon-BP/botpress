dfb45bfa95d6cabc10fc15ef86cb2d645c2802b8
Gordon-BP <gordynumberone@protonmail.com>
2025-08-08 14:08:46 +0800

    package.json
    Removing local files which merge conflicted

---
+++ integrations/mistral/integration.definition.ts
@@ -0,0 +1,25 @@
+/* bplint-disable */
+import { IntegrationDefinition, z } from '@botpress/sdk'
+// import { ModelId } from 'src/schemas'
+import * as llm from "@botpresshub/llm"
+
+export default new IntegrationDefinition({
+	name: 'google-ai',
+	title: 'Google AI',
+	description: 'Gain access to Gemini models for content generation, chat responses, and advanced language tasks.',
+	version: '6.0.3',
+	readme: 'hub.md',
+	icon: 'icon.svg',
+	entities: {
+		modelRef: {
+			schema: z.object({
+				id: z.string(),// ModelId,
+			}),
+		},
+	},
+	secrets: {
+		GOOGLE_AI_API_KEY: {
+			description: 'Google AI API key',
+		},
+	},
+}).extend(llm, ({ entities: { modelRef } }) => ({ entities: { modelRef } }))
